61356ec83ace997e7575a9502bd85f3cb786361e
Juan José Macanás <juanjo.macanas@gmail.com>
2014-03-26 16:41:26 +0100

    2

--- PruebaEgit/src/Clase_01.java
+++ PruebaEgit/src/Clase_01.java
@@ -2,7 +2,7 @@ public class Clase_01 {
 	@SuppressWarnings("unused")
 	private void inicio() {
 		
-		System.out.printf("Holaaa adios bye bye");
+		System.out.printf("Holaaa adios bye bye 2");
 	}
 
 }
